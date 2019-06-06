fe7719983c265de0123158e9c39e837c8bf4e41b
Marek Dano <mk.dano@gmail.com>
2019-06-06 16:02:39 +0100

    Add Store. Add socket.io-client to send and received messages in the app.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # chat-app
+
+## Description
+
+A Simple Chat App written in React which uses React Hooks, Material UI, Socket.io and Node.js.
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# client - watch mode
+$ yarn start
+
+# server
+$ node src/server
+```
+## Endpoints
+
+After running the project just open your browser and go to http://localhost:3000
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -8,6 +8,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+
+import { StoreContext } from './Store';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -39,6 +41,14 @@ const useStyles = makeStyles(theme => ({
 export default function Dashboard() {
   const classes = useStyles();
 
+  // StoreContext
+  const {allChats, sendChatAction, user} = useContext(StoreContext);
+  const topics = Object.keys(allChats);
+
+  // local state
+  const [activeTopic, setActiveTopic] = useState(topics[0]);
+  const [textValue, setTextValue] = useState('');
+  
   return (
     <div>
       <Paper className={classes.root}>
@@ -46,14 +56,14 @@ export default function Dashboard() {
           Chat app
         </Typography>
         <Typography variant="h5" component="h5">
-          Topic placeholder
+          {activeTopic}
         </Typography>
         <div className={classes.flex}>
           <div className={classes.topicWindow}>
             <List>
               {
-                ['topic'].map(topic => (
-                  <ListItem key={topic} button>
+                topics.map(topic => (
+                  <ListItem onClick={e => setActiveTopic(e.target.innerText)} key={topic} button>
                     <ListItemText primary={topic} />
                   </ListItem>
                 ))
@@ -63,10 +73,10 @@ export default function Dashboard() {
           <div className={classes.chatWindow}>
             <List>
               {
-                [{from: 'user', msg: 'hello'}].map((chat, i) => (
+                allChats[activeTopic].map((chat, i) => (
                   <div className={classes.flex} key={i}>
                     <Chip label={chat.from} className={classes.chip} />
-                    <Typography variant='p'>{chat.msg}</Typography>
+                    <Typography variant='body1'>{chat.msg}</Typography>
                   </div>
                 ))
               }      
@@ -77,8 +87,18 @@ export default function Dashboard() {
           <TextField
             label="Send a chat"
             className={classes.chatBox}
+            value={textValue}
+            onChange={(e) => setTextValue(e.target.value)}
           />
-          <Button variant="contained" color="primary" className={classes.button}>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            className={classes.button}
+            onClick={() => {
+              sendChatAction({from: user, msg: textValue, topic: activeTopic});
+              setTextValue('');
+            }}
+          >
             Send
           </Button>
         </div>
